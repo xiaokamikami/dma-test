c422f668c131b1a449fca6785b806b24d5b26162
Kami <fengkehan@bosc.ac.cn>
2024-09-05 19:03:48 +0800

    Add a sliding window version of the memory pool to support multi-channel packet-order testing

--- include/dma_mmpool.cpp
+++ include/dma_mmpool.cpp
@@ -6,11 +6,28 @@
 #include <functional>
 #define FIFO_SIZE 4096 * 1024 // 4M fifo
 #define PAGE_SIZE 4096  // 4K packge
-#define BLOCK_SIZE PAGE_SIZE * 32// one receive size 32KB
+#define BLOCK_SIZE PAGE_SIZE // one receive size 32KB
 #define NUM_BLOCKS (FIFO_SIZE / BLOCK_SIZE)
-#define REM_NUM_BLOCKS (NUM_BLOCKS - 1)
+#define REM_NUM_BLOCKS (NUM_BLOCKS - 1) //需要2的倍数
 
 extern bool running;
+    // 内存块结构
+    struct MemoryBlock {
+        std::unique_ptr<char, std::function<void(char*)>> data;
+        std::atomic<bool> is_free;
+
+        MemoryBlock() : is_free(true) {
+            void* ptr = nullptr;
+            if (posix_memalign(&ptr, 4096, 4096 + BLOCK_SIZE) != 0) {
+                throw std::runtime_error("Failed to allocate aligned memory");
+            }
+            data = std::unique_ptr<char, std::function<void(char*)>>(
+                static_cast<char*>(ptr),
+                [](char* p) { free(p); }
+            );
+        }
+    };
+
 class MemoryPool {
 public:
     // 构造函数，分配对齐的内存块
@@ -27,9 +44,7 @@ public:
 
     // 初始化内存池
     void initMemoryPool() {
-        memory_pool.reserve(NUM_BLOCKS);
         for (size_t i = 0; i < NUM_BLOCKS; ++i) {
-            memory_pool.emplace_back();
             block_mutexes[i].unlock();
         }
     }
@@ -38,7 +53,6 @@ public:
     void cleanupMemoryPool() {
         cv_empty.notify_all();
         cv_filled.notify_all();
-        memory_pool.clear();
     }
 
     void stopMemoryPool() {
@@ -86,23 +100,7 @@ public:
         ++empty_blocks;
     }
 private:
-    // 内存块结构
-    struct MemoryBlock {
-        std::unique_ptr<char, std::function<void(char*)>> data;
-        bool is_free;
-
-        MemoryBlock() : is_free(true) {
-            void* ptr = nullptr;
-            if (posix_memalign(&ptr, 4096, 4096 + BLOCK_SIZE) != 0) {
-                throw std::runtime_error("Failed to allocate aligned memory");
-            }
-            data = std::unique_ptr<char, std::function<void(char*)>>(
-                static_cast<char*>(ptr),
-                [](char* p) { free(p); }
-            );
-        }
-    };
-    std::vector<MemoryBlock> memory_pool;  // 内存池
+    MemoryBlock memory_pool[NUM_BLOCKS];  // 内存池
     std::vector<std::mutex> block_mutexes{NUM_BLOCKS}; // 分区锁数组
     std::atomic<size_t> empty_blocks = NUM_BLOCKS;      // 空闲块计数
     std::atomic<size_t> filled_blocks;     // 已填充块计数
@@ -112,4 +110,121 @@ private:
     std::condition_variable cv_filled;     // 已填充块条件变量
     size_t page_head = 0;
     size_t page_end = 0;
+};
+
+
+
+#define MAX_IDX 256
+#define MAX_GROUPING_IDX  NUM_BLOCKS / MAX_IDX
+#define REM_MAX_IDX  (MAX_IDX - 1)
+#define REM_MAX_GROUPING_IDX  (MAX_GROUPING_IDX - 1)
+
+// 根据索引宽度拆分内存池
+class MemoryIdxPool {
+public:
+    // 构造函数，分配对齐的内存块
+    MemoryIdxPool() {
+        initMemoryPool();
+    }
+
+    ~MemoryIdxPool() {
+        cleanupMemoryPool();
+    }
+    // 禁止拷贝构造函数和拷贝赋值操作符
+    MemoryIdxPool(const MemoryIdxPool&) = delete;
+    MemoryIdxPool& operator=(const MemoryIdxPool&) = delete;
+
+    // 初始化内存池
+    void initMemoryPool() {
+
+    }
+
+    // 清理内存池
+    void cleanupMemoryPool() {
+        cv_group.notify_all();
+    }
+
+    void stopMemoryPool() {
+        cv_group.notify_all();
+    }
+
+    // 获得一个空闲组的指定空闲块 返回空闲块的指针
+    char *get_free_chunk(uint8_t idx) {
+        write_index ++;
+        page_w_idx = idx + grouping_w_offset;
+        if (memory_pool[page_w_idx].is_free = false) {
+            printf("This block has been written, and there is a duplicate packge idx");
+            exit(-1);
+        }
+
+        // 进入到下一个分组
+        if (write_index == MAX_IDX) {
+            write_index = 0;
+            wait_next_free_group();
+            grouping_w_offset = (grouping_w_idx & REM_MAX_GROUPING_IDX) * write_index;
+        }
+        memory_pool[page_w_idx].is_free = false;
+        return memory_pool[page_w_idx].data.get();
+    }
+
+    // 获取最前面的内存
+    const char *get_busy_chunk() {
+        page_r_idx = read_index + grouping_r_offset;
+        if (read_index == MAX_IDX) {
+            read_index = 0;
+            wait_next_group();
+            grouping_r_offset = (grouping_r_idx & REM_MAX_GROUPING_IDX) * read_index;
+        }
+        read_index ++;
+        return memory_pool[page_r_idx].data.get();
+    }
+
+    void set_free_chunk() {
+        memory_pool[page_r_idx].is_free = true;
+    }
+
+    // 等待空闲内存块
+    bool wait_next_free_group() {
+        grouping_w_idx ++;
+        cv_group.notify_one();
+        if ((grouping_w_idx - grouping_r_idx) == (MAX_IDX - 1)) {
+            std::unique_lock<std::mutex> lock(block_mutexes);
+            printf("lock grouping_w_idx - grouping_r_idx \n");
+            cv_group.wait(lock, [this] { return (grouping_w_idx - grouping_r_idx) == (MAX_IDX - 1);});
+        }
+        return true;
+    }
+    // 等待数据可用
+    bool wait_next_group() {
+        if (grouping_w_idx == grouping_r_idx) {
+            std::unique_lock<std::mutex> lock(block_mutexes);
+            printf("lock grouping_w_idx == grouping_r_idx \n");
+            cv_group.wait(lock, [this] { return grouping_w_idx > grouping_r_idx;});
+        }
+        grouping_r_idx ++;
+        cv_group.notify_one();
+        return true;
+    }
+    //检查有无数据块可用
+    bool check_group() {
+        printf("w_idx %ld r_idx %ld\n",grouping_w_idx.load(), grouping_r_idx.load());
+        return (grouping_w_idx > grouping_r_idx) ? true : false;
+    }
+
+private:
+
+    MemoryBlock memory_pool[NUM_BLOCKS];  // 内存池
+    std::mutex block_mutexes;
+    std::condition_variable cv_group;      // 空闲条件变量
+    std::atomic<size_t> empty_blocks = NUM_BLOCKS;     // 空闲块计数
+    std::atomic<size_t> grouping_r_offset = 0; // 当前消费者使用的偏移量
+    std::atomic<size_t> grouping_w_offset = 0; // 当前生产者使用的偏移量
+    std::atomic<uint64_t> grouping_w_idx = 0;
+    std::atomic<uint64_t> grouping_r_idx = 0;
+    std::atomic<size_t> filled_blocks;     // 已填充块计数
+    std::atomic<size_t> write_index;       // 写入索引
+    std::atomic<size_t> read_index;        // 读取索引
+
+    size_t page_w_idx = 0;
+    size_t page_r_idx = 0;
 };
--- xdma/xdma_test.cpp
+++ xdma/xdma_test.cpp
@@ -14,17 +14,28 @@
 #include <errno.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
+#include <shared_mutex>
 
 #include "dma_mmpool.cpp"
 #include "diffstate.h"
 
 #define DEVICE_C2H_NAME "/dev/xdma0_c2h_"
 #define DEVICE_H2C_NAME "/dev/xdma0_h2c_"
-#define TEST_NUM (8000000ll / (BLOCK_SIZE / 4096))
+#define TEST_NUM (16000000ll / (BLOCK_SIZE / 4096))
+//#define TEST_NUM  255
 #define LOOP_BACK
 
-DiffTestState diffteststate;
-MemoryPool memory_pool[DMA_CHANNS];
+#define DMA_QUEUE_SIZE 32
+#define DMA_QUEUE_MAX_SIZE 255
+
+typedef struct {
+    uint8_t pack_indx;
+    DiffTestState diffteststate;
+} DmaPackge;
+DmaPackge test_packge;
+
+MemoryPool memory_pool;
+MemoryIdxPool memory_idx_pool;
 
 std::mutex test_mtx;
 std::condition_variable test_cv;
@@ -38,9 +49,8 @@ public:
         : running(false) {
 #ifdef HAVE_FPGA
     for(int i = 0; i < DMA_CHANNS;i ++) {
-        char c2h_device[64],h2c_device[64];
+        char c2h_device[64];
         sprintf(c2h_device,"%s%d",DEVICE_C2H_NAME,i);
-        sprintf(h2c_device,"%s%d",DEVICE_H2C_NAME,i);  
         xdma_c2h_fd[i] = open(c2h_device, O_RDONLY );
         if (xdma_c2h_fd[i] == -1) {
             std::cout << c2h_device << std::endl;
@@ -48,14 +58,16 @@ public:
             exit(-1);
         }
         std::cout << "XDMA link " << c2h_device << std::endl;
-        xdma_h2c_fd[i] = open(h2c_device, O_WRONLY);
-        if (xdma_h2c_fd[i] == -1) {
-            std::cout << h2c_device << std::endl;
-            perror("Failed to open XDMA device");
-            exit(-1);
-        }
-        std::cout << "XDMA link " << h2c_device << std::endl;
     }
+    char h2c_device[64];
+    sprintf(h2c_device,"%s%d",DEVICE_H2C_NAME,0);
+    xdma_h2c_fd = open(h2c_device, O_WRONLY);
+    if (xdma_h2c_fd == -1) {
+        std::cout << h2c_device << std::endl;
+        perror("Failed to open XDMA device");
+        exit(-1);
+    }
+    std::cout << "XDMA link " << h2c_device << std::endl;
 #endif
     }
 
@@ -67,10 +79,13 @@ public:
     // 启动流接收和处理
     void start() {
         if (running.exchange(true)) return; // 如果已经运行，直接返回
+        printf("DiffTestState Size %d \n",sizeof(DiffTestState));
         for(int i = 0; i < DMA_CHANNS;i ++) {
+            printf("start c2h channel %d \n", i);
             receive_thread[i] = std::thread(&StreamReceiver::receiveStream, this, i);
-            process_thread[i] = std::thread(&StreamReceiver::processData, this, i);
         }
+        process_thread = std::thread(&StreamReceiver::processData, this);
+        printf("start h2c\n");
     }
 
     // 停止流接收和处理
@@ -78,60 +93,96 @@ public:
         if (!running.exchange(false)) return; // 如果已经停止，直接返回
 
         // 通知线程停止
+        memory_pool.stopMemoryPool();
+        if (process_thread.joinable()) process_thread.join();
         for(int i = 0; i < DMA_CHANNS;i ++) {
-            memory_pool[i].stopMemoryPool();
             if (receive_thread[i].joinable()) receive_thread[i].join();
-            if (process_thread[i].joinable()) process_thread[i].join();
 #ifdef HAVE_FPGA
             close(xdma_c2h_fd[i]);
-            close(xdma_h2c_fd[i]);
 #endif
         }
+#ifdef HAVE_FPGA
+        close(xdma_h2c_fd);
+#endif
     }
 
 private:
     std::atomic<bool> running;   // 运行标志
 
     std::thread receive_thread[DMA_CHANNS];  // 接收线程
-    std::thread process_thread[DMA_CHANNS];  // 处理线程
+    std::thread process_thread;  // 处理线程
 
     int xdma_c2h_fd[DMA_CHANNS];             // XDMA文件描述符
-    int xdma_h2c_fd[DMA_CHANNS];
+    int xdma_h2c_fd;
+
+    // 生成测速数据包
+    void encapsulation_packge(DmaPackge *send_packg) {
+        static std::atomic<uint8_t> pack_indx = 0;
+        send_packg->pack_indx = pack_indx;
+        pack_indx ++;
+    }
 
     // 接收流数据
     void receiveStream(int channel) {
         #ifdef HAVE_FPGA
-            size_t w_bytes = write(xdma_h2c_fd[channel], "1", 1);
+            size_t w_bytes = write(xdma_h2c_fd, "1", 1);
         #endif
+        char *rdata = (char *)malloc(BLOCK_SIZE);
         while (running) {
-            char *memory = memory_pool[channel].get_free_chunk();
-        #ifdef HAVE_FPGA
-            size_t size = read(xdma_c2h_fd[channel], memory, BLOCK_SIZE);
-        #else
-            memset(memory, 1, BLOCK_SIZE);
+    #ifdef HAVE_FPGA
+            //uint64_t lseek_size = channel * sizeof(DmaPackge);
+            //lseek(xdma_c2h_fd[channel], lseek_size, SEEK_SET);
+            size_t size = read(xdma_c2h_fd[channel], rdata, sizeof(DmaPackge));
+        #ifdef LOOP_BACK
+            // 还原数据包
+            DmaPackge* packet = reinterpret_cast<DmaPackge*>(rdata); // rdata现在是指向包含DmaPackge的内存的指针  
+            uint8_t idx = packet->pack_indx;
+            //printf("get dma packge channel-%d idx %d \n", channel, idx);
+            char *memory = memory_idx_pool.get_free_chunk(idx);
+            memcpy(memory, &packet->diffteststate, sizeof(DmaPackge));
         #endif
-            memory_pool[channel].set_busy_chunk();
+    #else
+            memset(rdata, 1, BLOCK_SIZE);
+    #endif
         }
     }
 
     // 处理接收到的数据
-    void processData(int channel) {
+    void processData() {
+        static int get_diff_count = 0;
         while (running) {
-            const char *memory = memory_pool[channel].get_busy_chunk();
+        #if defined (HAVE_FPGA) && defined(LOOP_BACK)
+            DmaPackge send_packg[4];
+            encapsulation_packge(&send_packg[0]);encapsulation_packge(&send_packg[1]);
+            encapsulation_packge(&send_packg[2]);encapsulation_packge(&send_packg[3]);
+            write(xdma_h2c_fd, send_packg, sizeof(send_packg));
+            //printf("send pack id %d \n", send_packg.pack_indx);
+            if(memory_idx_pool.check_group() == true) {
+                const char *memory = memory_idx_pool.get_busy_chunk();
+                memcpy(&test_packge, memory, sizeof(DiffTestState));
+                printf("get difftest %d\n",get_diff_count);
+                get_diff_count ++;
+            }
 
-        #if defined (HAVE_FPGA) & defined(LOOP_BACK)
-            write(xdma_h2c_fd[channel], memory, BLOCK_SIZE);
         #else
-            memcpy(&diffteststate, memory, sizeof(diffteststate));
+            const char *memory = memory_pool.get_busy_chunk();
+            memcpy(&test_packge, memory, sizeof(DmaPackge));
+            memory_pool.set_free_chunk();
         #endif
 
             stream_receiver_cout ++;
-            memory_pool[channel].set_free_chunk();
+
             if (stream_receiver_cout >= TEST_NUM) {
                 printf("Reaching the statistical upper limit\n");
                 std::lock_guard<std::mutex> lock(test_mtx);
                 test_cv.notify_all();
             }
+        }
+    }
+
+    void sim_difftest() {
+        while(running) {
+
         }
     }
 };
@@ -147,8 +198,8 @@ int main() {
         {
             std::unique_lock<std::mutex> lock(test_mtx);
             test_cv.wait(lock, [&]() {return stream_receiver_cout >= TEST_NUM;});
+            clock_gettime(CLOCK_MONOTONIC, &ts_end);
         }
-        clock_gettime(CLOCK_MONOTONIC, &ts_end);
         receiver.stop();
 		// subtract the start time from the end time
 		double time_consum = (ts_end.tv_sec - ts_start.tv_sec) + 
